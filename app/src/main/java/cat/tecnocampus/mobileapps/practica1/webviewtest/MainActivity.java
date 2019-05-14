e4a1938a4997708da7220226c5944406b7e5aebc
lmartinezn <lmartinezn@edu.tecnocampus.cat>
2019-05-14 10:59:07 +0200

    Load web page with webview

--- app/src/main/java/cat/tecnocampus/mobileapps/practica1/webviewtest/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/practica1/webviewtest/MainActivity.java
@@ -2,6 +2,7 @@ package cat.tecnocampus.mobileapps.practica1.webviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView wv = findViewById(R.id.web_view);
+        wv.loadUrl("https://www.tecnocampus.cat");
     }
 }
